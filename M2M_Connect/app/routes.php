84792452cda5f26b9949991ac8143cb31d10cc50
alexm1751 <alex_mason1@hotmail.co.uk>
2018-01-10 11:12:07 +0000

    Basic file MVC file structure with components

---
+++ M2M_Connect/app/routes.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: alexmason
+ * Date: 10/01/2018
+ * Time: 11:07
+ */
---
+++ M2M_Connect_public/index.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: alexmason
+ * Date: 08/01/2018
+ * Time: 12:09
+ */
+include '../M2M_Connect/bootstrap.php';
