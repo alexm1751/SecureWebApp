7d18ec32e4042408313fe2c78ac4be6a894a07db
alexm1751 <alex_mason1@hotmail.co.uk>
2018-01-11 21:53:06 +0000

    SMSValidator creation

--- M2M_Connect/app/routes/processSmsConversion.php
+++ M2M_Connect/app/routes/processSmsConversion.php
@@ -17,17 +17,23 @@ $app->post(
         $sms_model = $this->get('sms_model');
 
         $messages = $sms_model->getUnreadMessages();
-        var_dump($messages);
-//        foreach ($messages as $message){
-//
-//            $xmlParser = $this->get('xml_parser');
-//
-//            $xmlParser = new xmlParser($message);
-//            $xmlParser->parse_the_xml_string();
-//            $parsedMessage = $xmlParser->get_parsed_data();
-//
-//            echo $parsedMessage;
-//            var_dump($parsedMessage);
+
+        foreach ($messages as $message){
+
+            $xml = simplexml_load_string($message);
+            if ($xml === false) {
+                echo "Failed loading XML: ";
+                foreach(libxml_get_errors() as $error) {
+                    echo "<br>", $error->message;
+                }
+            } else {
+                print_r($xml);
+            }
+
+        };
+
+
+
 
         return $this->view->render($response,
             'display_messages.html.twig',
@@ -38,6 +44,5 @@ $app->post(
                 'page_title' => APP_NAME,
                 'page_heading_1' => APP_NAME,
                 'page_heading_2' => 'Result',
-
             ]);
     });
--- M2M_Connect/app/src/SMSvalidator.php
+++ M2M_Connect/app/src/SMSvalidator.php
@@ -26,4 +26,111 @@ class SMSvalidator
         return $this->message[$Field];
     }
 
+    private function validateSourceMSISDN(){
+    $value = 'sourcemsisdn';
+
+    $tainted_Smsisdn = $this->getMessageItem($value);
+    $san_Smsisdn = filter_var($tainted_Smsisdn, FILTER_SANITIZE_NUMBER_INT);
+    $clean_Smsisdn = filter_var($san_Smsisdn, FILTER_VALIDATE_INT,array("options"=>array("min_range"=>0, "max_range"=>999999999999)));
+        //15 digits to meet Telecomunication Standardization Sector requirements for international numbers
+
+        if ($clean_Smsisdn === false) {
+            throw new FilterException($value);
+        }
+
+		return $clean_Smsisdn;
+
+    }
+
+    private function validateDestinationMSISDN(){
+        $value = 'destinationmsisdn';
+
+        $tainted_Dmsisdn = $this->getMessageItem($value);
+        $san_Dmsisdn = filter_var($tainted_Dmsisdn, FILTER_SANITIZE_NUMBER_INT);
+        $clean_Dmsisdn = filter_var($san_Dmsisdn, FILTER_VALIDATE_INT, array("options"=>array("min_range"=>0, "max_range"=>999999999999)));
+        //15 digits to meet Telecomunication Standardization Sector requirements for international numbers
+
+        if ($clean_Dmsisdn === false) {
+            throw new FilterException($value);
+        }
+
+		return $clean_Dmsisdn;
+
+    }
+
+    private function validateReceivedTime(){
+
+        $value = 'receivedtime';
+
+        $tainted_receivedTime = $this->getMessageItem($value);
+        $clean_receivedTime = filter_var($tainted_receivedTime, FILTER_SANITIZE_STRING);
+
+        if ($clean_receivedTime === false) {
+            throw new FilterException($value);
+        }
+
+
+        return $clean_receivedTime;
+
+    }
+
+    private function validateBearer(){
+
+        $value = 'bearer';
+
+        $tainted_bearer = $this->getMessageItem($value);
+        $clean_bearer = filter_var($tainted_bearer, FILTER_SANITIZE_STRING);
+
+
+        if ($clean_bearer === false){
+            throw new FilterException($value);
+        }
+
+        return $clean_bearer;
+
+    }
+
+    private function validateMessageRef(){
+        $value = 'messageref';
+
+        $tainted_messageref = $this->getMessageItem($value);
+        $san_messageref = filter_var($tainted_messageref, FILTER_SANITIZE_NUMBER_INT);
+        $clean_messageref = filter_var($san_messageref, FILTER_VALIDATE_INT);
+
+
+        if ($clean_messageref === false){
+            throw new FilterException($value);
+        }
+
+        return $clean_messageref;
+    }
+
+    private function validateMessageContent(){
+
+        $value = 'message';
+
+        $tainted_messageContent = $this->getMessageItem($value);
+        $clean_messageContent = filter_var($tainted_messageContent, FILTER_SANITIZE_STRING);
+
+        if ($clean_messageContent === false) {
+            throw  new FilterException($value);
+        }
+
+        return $clean_messageContent;
+
+}
+
+    public function validateMessages()
+    {
+        $sender = $this->validateSourceMSISDN();
+        $receiver = $this->validateDestinationMSISDN();
+        $date = $this->validateReceivedTime();
+        $brearer = $this->validateBearer();
+        $ref = $this->validateMessageRef();
+        $message = $this->validateMessageContent();
+
+        $newMessage = new messageDisplay($sender,$receiver,$date,$brearer,$ref,$message);
+
+        return$newMessage;
+    }
 }
