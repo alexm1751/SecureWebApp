90d168ac1b580f962a3462b8b2da819bddc936be
alexm1751 <alex_mason1@hotmail.co.uk>
2018-01-13 01:28:20 +0000

    DataBase initial Setup

---
+++ M2M_Connect/app/src/SQLQueries.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: alexmason
+ * Date: 13/01/2018
+ * Time: 01:04
+ */
+
+class SQLQueries
+{
+
+
+    public function __construct()
+    {
+    }
+
+    public function __destruct()
+    {
+    }
+
+    public static function get_user_details()
+    {
+
+    }
+
+    public static function get_stored_messages()
+    {
+
+    }
+}
